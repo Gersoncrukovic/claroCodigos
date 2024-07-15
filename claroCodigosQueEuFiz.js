b37b59245a7833663653e48e88388fc5a284be07
Gerson Vargas Crukovic <gvcrukovic@me.com>
2024-07-15 10:08:15 -0300

    Update claroCodigosQueEuFiz.js

--- claroCodigosQueEuFiz.js
+++ claroCodigosQueEuFiz.js
@@ -210,6 +210,49 @@ document.querySelector("#submit").addEventListener('click', function() {
 });
 </script>
 //-------------------------------------------------------------------------------------------------
+//sanitizado
+<script>
+function SetCookie(nomeCookie, valorCookie, expiracaoCookieDias) {
+    var dataExpiracao = new Date();
+    dataExpiracao.setTime(dataExpiracao.getTime() + (expiracaoCookieDias * 24 * 60 * 60 * 1000));
+    var expiracao = "expires=" + dataExpiracao.toUTCString();
+    
+    return document.cookie = nomeCookie + '=' + valorCookie + '; ' + expiracao + '; path=' + document.location.pathname + '; domain=' + document.domain;
+}
+
+function limparTelefone(telefone) {
+    return telefone.replace(/[()\-\s]/g, ''); // Remove parênteses, hífen e espaços
+}
+
+var telefone = document.querySelector("#phonenumber").value;
+telefone = limparTelefone(telefone);
+SetCookie('cookie_phone_teste', telefone, 1);
+
+</script>
+
+//nao sanitizado
+<script>
+function SetCookie(nomeCookie,valorCookie,expiracaoCookieDias ) {
+    var dataExpiracao = new Date();
+    dataExpiracao.setTime(dataExpiracao.getTime() + (expiracaoCookieDias * 24 * 60 * 60 * 1000));
+    var expiracao = "expires=" + dataExpiracao.toUTCString();
+    
+    return  document.cookie = nomeCookie+'='+valorCookie+'; expires='+expiracao + 'path='+document.location.pathname+'; domain='+ document.domain
+}
+
+SetCookie('cookie_telefone', document.querySelector("#phonenumber").value, 1);
+
+</script>
+/* */
+
+//----------------------------------------------------------------------------------------------------
+script simples pra adicionar algo ao datalayer 	
+<script>
+dataLayer.push({
+event: 'CliqueiNofone',
+tel: document.querySelector("#phonenumber").value
+})
+</script>
 
 
 
