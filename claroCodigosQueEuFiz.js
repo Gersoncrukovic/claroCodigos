096d1746a378bdd9c555014b4a96854f707bc339
Gerson Vargas Crukovic <gvcrukovic@me.com>
2024-07-16 11:42:01 -0300

    Update claroCodigosQueEuFiz.js

--- claroCodigosQueEuFiz.js
+++ claroCodigosQueEuFiz.js
@@ -254,6 +254,31 @@ tel: document.querySelector("#phonenumber").value
 })
 </script>
 
+//-----------------------------------------------------------------------------------------------
+armazena no local storage
+	
+html personalizado
+
+<script>
+  function a(){
+    var valor = document.querySelector("#phonenumber");
+    if(valor != null){
+    return valor;
+    }
+  }
+  localStorage.setItem("myCat", a());
+</script>
+
+variavel
+
+function(){
+  return window.localStorage.getItem('myCat');
+}
+function(){
+  var a = window.localStorage.getItem("myCat");
+  return a;
+}
+
 
 
 
