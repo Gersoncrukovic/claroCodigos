9205bcfbd752c871e75a30593a86796f906f4431
Gerson Vargas Crukovic <gvcrukovic@me.com>
2024-07-19 10:43:12 -0300

    Update claroCodigosQueEuFiz.js

--- claroCodigosQueEuFiz.js
+++ claroCodigosQueEuFiz.js
@@ -67,22 +67,11 @@ function(){
   }
 }
 
-//----------------------------------------------------------------------------------
-
-
 
 
 //-------------------------------------------------------------------------------------------
 
-//apaga umas merda que eu n quero ver na tela
-document.querySelector("#logo > ytd-yoodle-renderer > picture > img").style.display = "none";
 
-//--------------------------------------------------------------------------------------------
-
-// Impede a navegação ou fechamento
-window.addEventListener('beforeunload', (event) => {
-  event.preventDefault(); 
-});
 //---------------------------------------------------------------------------------------------
 
 document.querySelector("#btn-eu-quero-23283").addEventListener('click',function () {
