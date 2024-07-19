311c6e722eb5ae94d1713109e523f49bfa9b734a
Gerson Vargas Crukovic <gvcrukovic@me.com>
2024-07-19 10:42:30 -0300

    Update claroCodigosQueEuFiz.js

--- claroCodigosQueEuFiz.js
+++ claroCodigosQueEuFiz.js
@@ -31,9 +31,6 @@ function(){
   }
 }
 
-//------------------------------------------------------------------------------------------------------------------------------------------
-
-
 
 //---------------------------------------------------------------------------------
 
@@ -72,16 +69,7 @@ function(){
 
 //----------------------------------------------------------------------------------
 
-//Ve se o navegadordo cliente é dark ou light 
-function () {
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      return 'Dark';
-    }
-    else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
-      return 'Light';
-    }
-    return 'No Preference';
-  }
+
 
 
 //-------------------------------------------------------------------------------------------
