d7638fb97f94deb6ef8a00fdb0fd39af701efe69
Gerson Vargas Crukovic <gvcrukovic@me.com>
2024-07-19 10:45:48 -0300

    Update claroCodigosQueEuFiz.js

--- claroCodigosQueEuFiz.js
+++ claroCodigosQueEuFiz.js
@@ -69,9 +69,6 @@ function(){
 
 
 
-//-------------------------------------------------------------------------------------------
-
-
 //---------------------------------------------------------------------------------------------
 
 document.querySelector("#btn-eu-quero-23283").addEventListener('click',function () {
@@ -99,41 +96,6 @@ document.querySelector("#submit").addEventListener('click', function() {
     }
 });
 </script>
-//-------------------------------------------------------------------------------------------------
-//sanitizado
-<script>
-function SetCookie(nomeCookie, valorCookie, expiracaoCookieDias) {
-    var dataExpiracao = new Date();
-    dataExpiracao.setTime(dataExpiracao.getTime() + (expiracaoCookieDias * 24 * 60 * 60 * 1000));
-    var expiracao = "expires=" + dataExpiracao.toUTCString();
-    
-    return document.cookie = nomeCookie + '=' + valorCookie + '; ' + expiracao + '; path=' + document.location.pathname + '; domain=' + document.domain;
-}
-
-function limparTelefone(telefone) {
-    return telefone.replace(/[()\-\s]/g, ''); // Remove parênteses, hífen e espaços
-}
-
-var telefone = document.querySelector("#phonenumber").value;
-telefone = limparTelefone(telefone);
-SetCookie('cookie_phone_teste', telefone, 1);
-
-</script>
-
-//nao sanitizado
-<script>
-function SetCookie(nomeCookie,valorCookie,expiracaoCookieDias ) {
-    var dataExpiracao = new Date();
-    dataExpiracao.setTime(dataExpiracao.getTime() + (expiracaoCookieDias * 24 * 60 * 60 * 1000));
-    var expiracao = "expires=" + dataExpiracao.toUTCString();
-    
-    return  document.cookie = nomeCookie+'='+valorCookie+'; expires='+expiracao + 'path='+document.location.pathname+'; domain='+ document.domain
-}
-
-SetCookie('cookie_telefone', document.querySelector("#phonenumber").value, 1);
-
-</script>
-/* */
 
 //----------------------------------------------------------------------------------------------------
 	
