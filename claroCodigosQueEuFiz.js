7e70b48e3c0f7847e9fb3e4ea4f6950e314e580a
Gerson Vargas Crukovic <gvcrukovic@me.com>
2024-07-19 10:40:33 -0300

    Update claroCodigosQueEuFiz.js

--- claroCodigosQueEuFiz.js
+++ claroCodigosQueEuFiz.js
@@ -1,42 +1,4 @@
-//teste de push manual
-dataLayer.push({
-	'event': 'purchase',
-	'eventCategory': 'TESTE',
-	'eventAction': 'TESTE',
-	'eventLabel': 'TESTE',
-	'ecommerce': {
-		'purchase': {
-			'actionField': {
-				'id': 'TESTEERRE4545',	
-				'revenue': '00.00',
-			},
-			'products': [{
-				'name': 'TESTE1',			
-				'id': 'PRD122',
-				'price': '00.00',
-				'brand': 'TESTE',
-				'category': 'TESTE/TVTESTE',
-				'quantity': '1',
-			},
-        {
-				'name': 'TESTE2',			
-				'id': 'PRD2222',
-				'price': '00.00',
-				'brand': 'TESTE',
-				'category': 'TESTE/TESTEINTERNET',
-				'quantity': '1',
-			},
-        {
-				'name': 'TESTE3',			
-				'id': 'PRD2232',
-				'price': '00.00',
-				'brand': 'TESTE',
-				'category': 'TESTE/selecoes',
-				'quantity': '1',
-			}]
-		}
-	}
-});
+
 
 
 //------------------------------------------------------------------------------------------------------------------------------------------
